55bb0753856b295a4fb364dd28bbf6be65cd3d39
Jhennifer Bueno <jhenni.bueno@gmail.com>
2024-09-05 20:53:27 -0300

    continuação aula 02

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Opa from './Opa'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const nome = "Valdair"
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Olá, {nome}</h1>
+      <h2>Hora agora: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+
+  root.render(element)
+}
+
+
+
+setInterval(tick, 1000);
+
+
+
+// root.render(
+//   <React.StrictMode>
+//     <Opa />
+//     <App />   
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
