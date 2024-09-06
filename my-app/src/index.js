5c6101e23b7b4c19abb9462943142bf04e55a0ee
Jhennifer Bueno <jhenni.bueno@gmail.com>
2024-09-05 21:30:58 -0300

    atividades

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Opa from './Opa'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nome = "Jhennifer"
+const nome = "Lista de Tarefas"
 
 
 function tick() {
   const element = (
     <div>
-      <h1>Olá, {nome}</h1>
-      <h2>Hora agora: {new Date().toLocaleTimeString()}</h2>
+       <h3>Hora agora: {new Date().toLocaleTimeString()}</h3>
+      <h1> {nome}</h1>
+      <ul>
+       <li><i>Retirar o lixo</i></li> 
+       <li><i>Levar as crianças ao médico</i></li> 
+       <li><i>reunião as 14:30</i></li> 
+       <li><i>Pagar contas</i></li> 
+       <li><i>Levar o carro para revisão</i></li> 
+
+      </ul>
+     
     </div>
   );
 
   root.render(element)
 }
 
-
-
 setInterval(tick, 1000);
+
+
+
 
 
 
