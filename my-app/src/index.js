2081b2bd16e1b5a7141a72cd27341f177581e948
Jhennifer Bueno <jhenni.bueno@gmail.com>
2024-09-05 21:06:57 -0300

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nome = "Valdair"
+const nome = "Jhennifer"
 
 
 function tick() {
