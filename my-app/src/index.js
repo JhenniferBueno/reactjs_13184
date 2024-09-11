272f4e708fa401d28eda14aaea767693e5ea8fcf
Jhennifer Bueno <jhenni.bueno@gmail.com>
2024-09-11 19:27:49 -0300

    estudo de lab

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,31 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import ButtonEvent from './ButtonEvent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nome = "Lista de Tarefas"
+root.render(
+  <div>
+    <ButtonEvent></ButtonEvent>
+  </div>
+)
 
 
-function tick() {
-  const element = (
-    <div>
-       <h3>Hora agora: {new Date().toLocaleTimeString()}</h3>
-      <h1> {nome}</h1>
-      <ul>
-       <li><i>Retirar o lixo</i></li> 
-       <li><i>Levar as crianças ao médico</i></li> 
-       <li><i>reunião as 14:30</i></li> 
-       <li><i>Pagar contas</i></li> 
-       <li><i>Levar o carro para revisão</i></li> 
-
-      </ul>
-     
-    </div>
-  );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
   root.render(element)
-}
+
 
 setInterval(tick, 1000);
 
